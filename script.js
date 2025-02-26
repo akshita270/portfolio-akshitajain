5f53722cae66d47cef9d7776c07d04d28400459a
Akshita jain <85305338+akshita270@users.noreply.github.com>
2025-02-26 14:25:52 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Welcome to my portfolio!');
+    // Toggle mobile navigation
+document.querySelector(".hamburger").addEventListener("click", function() {
+    document.querySelector(".nav-links").classList.toggle("active");
+});
+
+});
+
